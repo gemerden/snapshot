8f9d6cb8e34630287021ecf10aabc31405b18ed4
gemerden <gemerden@gmail.com>
2021-08-08 15:16:33 +0200

    mini refactor

--- snapshot/unittests/test_snapshot.py
+++ snapshot/unittests/test_snapshot.py
@@ -46,7 +46,7 @@ class testSnapShot(TestCase):
         assert snap.x == 4
 
     def test_combined(self):
-        class Some(object):
+        class Some:
             snapshot = Snapshot('a', b='b', c=lambda obj: obj.c, s=lambda obj: obj.d ** 2, ea='e.a')
 
             def __init__(self, a, b=None, c=None, d=None, e=None):
@@ -66,14 +66,14 @@ class testSnapShot(TestCase):
 
     def test_call(self):
         class Some:
-            snapshot = Snapshot('a')
+            snapshot = Snapshot(x='a')
 
             def __init__(self, a):
                 self.a = a
 
         somes = [Some(i) for i in range(10)]
         snaps = Some.snapshot(somes)  # returns iterator
-        assert [s.a for s in snaps] == list(range(10))
+        assert [s.x for s in snaps] == list(range(10))
 
     def test_exceptions(self):
         with self.assertRaises(TypeError):
